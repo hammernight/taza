cb90601ac3ff060e5931db0659f2f44437bbe70d
Arik Jones <tnypxl00@icloud.com>
2017-07-28 17:25:54 -0500

    Remove unused options in Taza::Options

--- lib/taza/options.rb
+++ lib/taza/options.rb
@@ -5,17 +5,13 @@ module Taza
 
     def add_sources(builder)
 #     builder.add_source(CommandLineSource, :usage, "Usage: ruby #{$0} [options] file1 [file2]")
-      builder.add_source(EnvironmentSource, :mapping, {:browser => 'BROWSER', :driver => 'DRIVER', :attach => 'ATTACH', :timeout => 'TIMEOUT', :server_ip => 'SERVER_IP', :server_port => 'SERVER_PORT'})
+      builder.add_source(EnvironmentSource, :mapping, {:browser => 'BROWSER', :driver => 'DRIVER'})
       builder.add_source(YamlConfigFileSource, :from_complete_path, Settings.config_file_path)
     end
 
     def add_choices(builder)
      builder.add_choice(:browser, :type=>:string, :default=>'firefox')
-     builder.add_choice(:driver, :type=>:string, :default=>'selenium')
-     builder.add_choice(:attach, :type=>:boolean, :default=>false)
-     builder.add_choice(:timeout, :type=>:string)
-     builder.add_choice(:server_ip, :type=>:string)
-     builder.add_choice(:server_port, :type=>:string)
+     builder.add_choice(:driver, :type=>:string, :default=>'watir')
     end
 
     def execute
