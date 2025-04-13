a89685e79a6b34438cb3e0d647e3725988dd3aee
uchagani <umair.chagani@gmail.com>
2025-04-12 22:25:55 -0400

    Fix tests

--- spec/taza/site_fixtures_spec.rb
+++ spec/taza/site_fixtures_spec.rb
@@ -1,14 +1,18 @@
 require 'spec_helper'
 describe "Site Specific Fixtures" do
-  Taza::Fixture.stubs(:base_path).returns(File.join('.','spec','sandbox','fixtures',''))
-  Taza.load_fixtures
-  include Taza::Fixtures::FooSite
+
+  before do
+    stub_path = File.join(@original_directory, 'spec', 'sandbox', 'fixtures', '')
+    Taza::Fixture.stubs(:base_path).returns(stub_path)
+    Taza.load_fixtures
+    self.class.include(Taza::Fixtures::FooSite)
+  end
 
   it "should be able to access fixtures in sub-folders" do
     expect(bars(:foo).name).to eql 'foo'
   end
 
   it "should not be able to access non-site-specific fixtures" do
-    expect(lambda{foos(:gap)}).to raise_error(NoMethodError)
+    expect(lambda { foos(:gap) }).to raise_error(NoMethodError)
   end
 end
